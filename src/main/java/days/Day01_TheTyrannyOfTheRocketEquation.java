13f3e9066f2d42adeb104d730fd0e342beb32637
Paul Queruel <paul.queruel@viacesi.fr>
2019-12-03 15:03:36 +0100

    Added day 1, part 1 & 2

---
+++ src/main/java/days/Day01_TheTyrannyOfTheRocketEquation.java
@@ -0,0 +1,53 @@
+package days;
+
+import launcher.ChallengeHelper;
+import launcher.Executable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day01_TheTyrannyOfTheRocketEquation implements Executable {
+    @Override
+    public String executePartOne() {
+        List<String> input = ChallengeHelper.readInputData(1);
+        // Cast strings to doubles to be able to divide it and keep precision
+        List<Double> inputAsDoubles = input.stream()
+            .mapToDouble(Double::parseDouble)
+            .boxed()
+            .collect(Collectors.toList());
+
+        return String.valueOf(
+            inputAsDoubles
+            .stream()
+            .mapToDouble(fuel -> Math.floor(fuel / 3) - 2)
+            .sum()
+        );
+    }
+
+    @Override
+    public String executePartTwo() {
+        List<String> input = ChallengeHelper.readInputData(1);
+        List<Double> inputAsDoubles = input.stream()
+            .mapToDouble(Double::parseDouble)
+            .boxed()
+            .collect(Collectors.toList());
+
+        return String.valueOf(
+            inputAsDoubles
+                .stream()
+                .mapToDouble(fuel -> computeFuelRequirements(fuel, 0))
+                .sum()
+        );
+    }
+
+    private double computeFuelRequirements(double fuel, double sum) {
+        double fuelRequirement = Math.floor(fuel / 3) - 2;
+
+        // If the fuel requirement is negative or zero, we should halt there
+        if (fuelRequirement <= 0) {
+            return sum;
+        } else {
+            return computeFuelRequirements(fuelRequirement, sum + fuelRequirement);
+        }
+    }
+}
